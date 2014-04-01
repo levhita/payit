4f0474fa7e2bc8c4b09f8b74ea261542665366a9
Argel Arias <levhita@gmail.com>
2014-04-01 11:17:54 -0600

    setup

---
+++ application/config/twitter.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$config['twitter_consumer_token']		= 'XBhPK0SFXjz6Kf5lN4FW8U5L5';
+$config['twitter_consumer_secret']		= 'pRE4MzVGvumoMjKR5ORdk4FrUlG0OD9dq4tgGkXQckokYvKnjm';
+$config['twitter_access_token']			= 'your_access_token'; // Optional
+$config['twitter_access_secret']		= 'your_access_token_secret'; // Optional
+
+/* End of file twitter.php */
+/* Location: ./application/config/twitter.php */
